649d2ef926f489a91066c5deba6d2f1fe63a31f2
Stephen J. Eglen <sje30@cam.ac.uk>
2021-05-22 11:53:12 +0100

    fix up script to work with other csvs

--- data/clean.sh
+++ data/clean.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-sed 's/"\([0-9]*\),\([0-9.]*\) "/\1\2/g' $1 | sed 's/ ,/,/g' 
+sed 's/"\(-\?[0-9]\+\),\([0-9.]*\) \?"/\1\2/g' $1 | sed 's/ ,/,/g'
+
 
 ## remove the quotes and commas around prices, then remove spaces before commas.
+
+# -\?   will match  zero or one minus sign
+#  \?   will match zero or one space at end of number
+# Sometimes there is a space trailing the number, sometimes not.
